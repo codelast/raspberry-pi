c23abab53db877b5d6168339fc20bb0a65b0fbdb
Darran Zhang <lhcsdn@gmail.com>
2015-03-10 23:26:24 +0800

    Add program & related shell script to demonstrate how to detect motion using OpenCV.

---
+++ webcam/motion-detection/detect-motion-using-opencv.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# A script to invoke a C program to detect motion in front of an USB camera using OpenCV.
+
+CURRENT_DIR=`dirname "$0"`
+WORKING_HOME=`cd "$CURRENT_DIR"; pwd`
+
+SOURCE_FILE=$WORKING_HOME/motion-detection.c
+BIN_FILE=$WORKING_HOME/motion-detection
+
+# compile the source code if the executable bin not exists
+if [ ! -f $BIN_FILE ]; then
+    echo "Compiling $SOURCE_FILE ..."
+    g++ -I/usr/include/opencv2/  `pkg-config --cflags opencv --libs opencv` $SOURCE_FILE -o $BIN_FILE
+    if [ $? -ne 0 ]; then
+	echo "Failed to compile $SOURCE_FILE"
+	exit 1
+    fi
+fi
+
+# run the program
+$BIN_FILE
---
+++ webcam/motion-detection/motion-detection.c
@@ -0,0 +1,93 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include "opencv.hpp"
+
+/**
+ * A program to detect motion in front of an USB camera using OpenCV.
+ *
+ * @author Darran Zhang @ codelast.com
+ */
+
+int main(int argc, char **argv) {
+  int detectThreshold = 19;
+  if (argc >= 2) {
+    detectThreshold = atoi(argv[1]);
+    printf("=== Motion detection threshold has been set to: [%d] ===\n", detectThreshold);
+  }
+
+  /* init camera */
+  CvCapture* pCapture = cvCreateCameraCapture(0);
+  cvSetCaptureProperty(pCapture, CV_CAP_PROP_FRAME_WIDTH, 640);
+  cvSetCaptureProperty(pCapture, CV_CAP_PROP_FRAME_HEIGHT, 480);
+  cvSetCaptureProperty(pCapture, CV_CAP_PROP_BRIGHTNESS, 20);
+  cvSetCaptureProperty(pCapture, CV_CAP_PROP_CONTRAST, 10);
+
+  if (NULL == pCapture) {
+    fprintf(stderr, "Can't initialize webcam!\n");
+    return 1;
+  }
+
+  IplImage *pFrame = cvQueryFrame(pCapture);
+  IplImage *pFrameA = NULL;
+  IplImage *pFrameB = cvCreateImage(cvSize(pFrame->width, pFrame->height), pFrame->depth, pFrame->nChannels);
+  IplImage *pFrameSub = cvCloneImage(pFrameB);
+ 
+  int nDims = 256;
+  float hRangesArr[] = {0, 255};
+  float* hRanges = hRangesArr;
+ 
+  IplImage *pGrayscaleImage = NULL;
+  CvHistogram *pHist = cvCreateHist(1, &nDims, CV_HIST_ARRAY, &hRanges, 1);
+  float fMaxValue = 0.0;
+
+  time_t ts = 0;  // used to record current timestamp & prevent triggering the monition detection for multi times in 1 second
+  while(true)
+    {
+      pFrameA = cvQueryFrame(pCapture);
+      if(!pFrameA) {
+	fprintf(stdout, "Can't grab images!\n");
+ 
+	break;
+      }
+ 
+      cvAbsDiff(pFrameB, pFrameA, pFrameSub);
+      cvCopy(pFrameA, pFrameB);
+ 
+      pGrayscaleImage = cvCreateImage(cvGetSize(pFrameSub), IPL_DEPTH_8U, 1);
+      cvCvtColor(pFrameSub, pGrayscaleImage, CV_BGR2GRAY);
+      cvCalcHist(&pGrayscaleImage, pHist, 0, 0);
+
+      fMaxValue = 0.0;
+      cvGetMinMaxHistValue(pHist, 0, &fMaxValue, 0, 0);
+      cvConvertScale(pHist->bins, pHist->bins, (fMaxValue ? (255.0 / fMaxValue) : 0.0), 0);
+ 
+      double dRealtimeVal = cvGetReal1D(pHist->bins, 10);
+      if (dRealtimeVal > detectThreshold) {
+	time_t currentTimestamp = time(NULL);
+	if (currentTimestamp - ts >= 1) {
+	  ts = currentTimestamp;
+	  fprintf(stdout, "Motion detected!\n");
+	}
+      }
+ 
+      cvReleaseImage(&pGrayscaleImage);
+      pGrayscaleImage = NULL;
+ 
+      cvWaitKey(10);
+    }
+ 
+  cvReleaseCapture(&pCapture);
+  cvReleaseHist(&pHist);
+  cvReleaseImage(&pFrameB);
+  cvReleaseImage(&pFrameSub);
+ 
+  pCapture = NULL;
+  pHist = NULL;
+  pFrameB = NULL;
+  pFrameSub = NULL;
+  pFrame = NULL;
+  pFrameA = NULL;
+
+  return 0;
+}
