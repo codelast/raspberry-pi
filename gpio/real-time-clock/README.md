fb9cf57b12f137a54449a3f45ef07f5d8f7fb29e
Darran Zhang <lhcsdn@gmail.com>
2015-11-01 10:31:53 +0800

    Add a program to work with the DS1302 real-time clock

---
+++ gpio/real-time-clock/README.md
@@ -0,0 +1,1 @@
+Slight modification to the ds1302.c shipped with WiringPi to work with the DS1302 Real-Time Clock module.
---
+++ gpio/real-time-clock/ds1302.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+# A script to run the real-time clock manipulation program.
+
+CURRENT_DIR=`dirname "$0"`
+WORKING_HOME=`cd "$CURRENT_DIR"; pwd`
+
+echo "Usage:./ds1302.sh [-slc | -sdsc | -rtest]"
+
+PARAM=
+if [ $# -ge 1 ]; then
+    PARAM=$1
+fi
+
+# set these ports according to your RPi GPIO pinout
+SCLK_PORT=14
+SDA0_PORT=8
+CE0_PORT=10
+
+SOURCE=$WORKING_HOME/ds1302.c
+BIN=$WORKING_HOME/ds1302
+
+# compile the source code if the executable bin not exists
+if [ ! -f $BIN ]; then
+    echo "Compiling $SOURCE ..."
+    gcc $SOURCE -o $BIN -lwiringPi -lwiringPiDev
+    if [ $? -ne 0 ]; then
+	echo "Failed to compile $SOURCE"
+	exit 1
+    fi
+fi
+
+# run the program
+$BIN $SCLK_PORT $SDA0_PORT $CE0_PORT $PARAM
