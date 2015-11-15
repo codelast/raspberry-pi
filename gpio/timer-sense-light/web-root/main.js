87c39e10aa107d19ba011bfff613a973ab1599aa
Darran Zhang <lhcsdn@gmail.com>
2015-11-15 16:54:32 +0800

    Now the web UI can add time range input box via a button(but the backend logic is not done yet)

--- gpio/timer-sense-light/web-root/main.js
+++ gpio/timer-sense-light/web-root/main.js
@@ -1,4 +1,8 @@
 $(document).ready(function() {
+    var maxTimeRangeInputBoxNumber = 10;    // maximum input boxes allowed
+    var wrapper = $(".input_fields_wrap");  // fields wrapper
+    var timeRangeInputBoxNumber = 1;        // initlal input box number
+
     // start 5-second timer to call RESTful endpoint
     setInterval(function() {
 	$.ajax({
@@ -10,6 +14,22 @@ $(document).ready(function() {
 	});
     }, 5000);
 
+    $('.js__timepicker').pickatime();
+
+    $("#add_time_range").click(function(e) {
+	e.preventDefault();
+	if(timeRangeInputBoxNumber < maxTimeRangeInputBoxNumber) {   // max input box allowed
+	    timeRangeInputBoxNumber++;  // text box increment
+	    $(wrapper).append('<div><input class="js__timepicker" type="text" size="10"/><label> ~ </label><input class="js__timepicker" type="text" size="10"/><a href="#" class="remove_field">Remove</a></div>');  // add input box
+	} else {
+	    alert('Cannot add more');
+	}
+    });
+
+    $(wrapper).on("click",".remove_field", function(e){  // user click on remove text
+	e.preventDefault(); $(this).parent('div').remove(); x--;
+    });
+
     $("#light_mode_on").click(function() {
 	$.ajax({
 	    url: '/switch-mode-on',
@@ -19,8 +39,6 @@ $(document).ready(function() {
 	    }
 	});
     });
-
-    $('.js__timepicker').pickatime();
     
     $("#light_mode_off").click(function() {
 	$.ajax({
