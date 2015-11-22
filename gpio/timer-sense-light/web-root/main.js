a5e65bd59a9c6865b77244c7ec430b03eb6107aa
Darran Zhang <lhcsdn@gmail.com>
2015-11-22 10:16:18 +0800

    Remove some unused codes

--- gpio/timer-sense-light/web-root/main.js
+++ gpio/timer-sense-light/web-root/main.js
@@ -115,34 +115,4 @@ $(document).ready(function() {
 	    }
 	});
     });
-
-    $("#light_mode_on").click(function() {
-	$.ajax({
-	    url: '/switch-mode-on',
-	    success: function() {
-		$("#light_mode_on").prop('disabled', true);    // disable button
-		$("#light_mode_off").prop('disabled', false);  // enable button
-	    }
-	});
-    });
-    
-    $("#light_mode_off").click(function() {
-	$.ajax({
-	    url: '/switch-mode-off',
-	    success: function() {
-		$("#light_mode_off").prop('disabled', true);  // disable button
-		$("#light_mode_on").prop('disabled', false);  // enable button
-	    }
-	});
-    });
-
-    $("#light_mode_auto").click(function() {
-	$.ajax({
-	    url: '/switch-mode-auto',
-	    success: function() {
-		$("#light_mode_on").prop('disabled', false);   // enable button
-		$("#light_mode_off").prop('disabled', false);  // enable button
-	    }
-	});
-    });
 });
