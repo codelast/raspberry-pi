b674e51c0b8a77835594396d1828d00e20125612
Darran Zhang <lhcsdn@gmail.com>
2015-12-06 17:18:58 +0800

    Add some test cases

--- gpio/timer-sense-light/include/test/util_test.h
+++ gpio/timer-sense-light/include/test/util_test.h
@@ -7,6 +7,7 @@
 #include <sys/types.h>     // mkdir()
 #include <stdlib.h>        // system()
 #include <stdio.h>         // remove()
+#include "constants.h"
 #include "util.h"
 
 /**
@@ -17,61 +18,76 @@
 
 using namespace std;
 
-class CUtilTest : public testing::Test
-{
- protected:
-  string toCreateDir;
+namespace {
+  class CUtilTest : public testing::Test
+  {
+  protected:
+    string toCreateDir;
   
- protected:
-  virtual void SetUp() {
-    /* get current running program path */
-    string currentAppPath;
-    char path[PATH_MAX];
-    memset(path, 0, sizeof(path));
-    if (-1 != (int) CUtil::getExecutablePath(path, sizeof(path))) {
-      currentAppPath = path;
+  protected:
+    virtual void SetUp() {
+      /* get current running program path */
+      string currentAppPath;
+      char path[PATH_MAX];
+      memset(path, 0, sizeof(path));
+      if (-1 != (int) CUtil::getExecutablePath(path, sizeof(path))) {
+	currentAppPath = path;
+      }
+      toCreateDir = currentAppPath + "to-create-dir-for-ut";
+
+      /* remove the dir before creating it(if it exists) */
+      string shellCommand = "rm -rf " + toCreateDir;
+      system(shellCommand.c_str());
     }
-    toCreateDir = currentAppPath + "to-create-dir-for-ut";
-
-    /* remove the dir before creating it(if it exists) */
-    string shellCommand = "rm -rf " + toCreateDir;
-    system(shellCommand.c_str());
-  }
   
-  virtual void TearDown() {
-    remove(toCreateDir.c_str());
+    virtual void TearDown() {
+      remove(toCreateDir.c_str());
+    }
+  };
+
+  TEST(isDirExistTest, givenNonExistDirShouldReturnFalse) {
+    EXPECT_FALSE(CUtil::isDirExist("a-non-exist-dir"));
   }
-};
 
-TEST_F(CUtilTest, isDirExistTestGivenExistingDirShouldReturnTrue) {
-  /* create a temp directory for unit test */
-  int ret = mkdir(toCreateDir.c_str(), S_IRWXU | S_IRWXG | S_IROTH | S_IXOTH);
-  ASSERT_EQ(0, ret) << "Failed to create dir [" << toCreateDir << "], return value: " << ret;
+  TEST_F(CUtilTest, givenExistingDirShouldReturnTrue) {
+    /* create a temp directory for unit test */
+    int ret = mkdir(toCreateDir.c_str(), S_IRWXU | S_IRWXG | S_IROTH | S_IXOTH);
+    ASSERT_EQ(0, ret) << "Failed to create dir [" << toCreateDir << "], return value: " << ret;
   
-  EXPECT_TRUE(CUtil::isDirExist(toCreateDir));
-}
+    EXPECT_TRUE(CUtil::isDirExist(toCreateDir));
+  }
 
-TEST(isDirExistTest, isDirExistTestGivenNonExistDirShouldReturnFalse) {
-  EXPECT_FALSE(CUtil::isDirExist("a-non-exist-dir"));
-}
+  TEST(stringSplitTest, givenEmptyInputStringShouldGetEmptyOutput) {
+    string input = "";
+    vector<string> output;
+    CUtil::stringSplit(input, '\t', output);
 
-TEST(stringSplitTest, givenNonEmptyInputStringShouldSplitToSubStrings) {
-  string input = "a\tb\tc";
-  vector<string> output;
-  CUtil::stringSplit(input, '\t', output);
+    EXPECT_EQ(0, output.size());
+  }
 
-  EXPECT_EQ(3, output.size());
-  EXPECT_STREQ("a", output[0].c_str());
-  EXPECT_STREQ("b", output[1].c_str());
-  EXPECT_STREQ("c", output[2].c_str());
-}
+  TEST(stringSplitTest, givenNonEmptyInputStringShouldSplitToSubStrings) {
+    string input = "a\tb\tc";
+    vector<string> output;
+    CUtil::stringSplit(input, '\t', output);
 
-TEST(stringSplitTest, givenEmptyInputStringShouldGetEmptyOutput) {
-  string input = "";
-  vector<string> output;
-  CUtil::stringSplit(input, '\t', output);
+    EXPECT_EQ(3, output.size());
+    EXPECT_STREQ("a", output[0].c_str());
+    EXPECT_STREQ("b", output[1].c_str());
+    EXPECT_STREQ("c", output[2].c_str());
+  }
 
-  EXPECT_EQ(0, output.size());
+  TEST(getPositionInTimeRangeTest, givenValidInputStringShouldReturnInvalidFlag) {
+    string hourAndMinute("123");
+    EXPECT_EQ(INVALID_POSITION, CUtil::getPositionInTimeRange(hourAndMinute));
+  }
+
+  TEST(getPositionInTimeRangeTest, givenInvalidInputStringShouldReturnRightPosition) {
+    string hourAndMinute("00:05");
+    EXPECT_EQ(5, CUtil::getPositionInTimeRange(hourAndMinute));
+
+    hourAndMinute = "10:20";
+    EXPECT_EQ(620, CUtil::getPositionInTimeRange(hourAndMinute));
+  }
 }
 
 #endif
